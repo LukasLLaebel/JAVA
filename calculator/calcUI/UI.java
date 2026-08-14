ce272f20b9ae6a4f6c211bcccb4621c3ac3510c3
lukas leander laebel <lukaslaebel@gmail.com>
2026-08-14 20:43:01 +0200

    adding centered title, some button structure and an display text

--- calculator/calcUI/UI.java
+++ calculator/calcUI/UI.java
@@ -19,20 +19,37 @@ public class UI {
     frame.setResizable(false);
 
     // labels
-    JLabel title = new JLabel("Enter first number:");
+    JLabel title = new JLabel("Calculator");
+
     int titleWidth = 300;
     int titleHeight = 50;
-    title.setBounds(titleWidth / 2, 15, titleWidth, titleHeight);
+    int titleX = (windowWidth - titleWidth) / 2;
+
+    title.setBounds(titleX, 15, titleWidth, titleHeight);
+    title.setHorizontalAlignment(SwingConstants.CENTER);
+    title.setFont(new Font("SansSerif", Font.PLAIN, 50));
+
     frame.add(title); // add labels to frame
-    title.setFont(new Font("SansSerif", Font.PLAIN, 20));
+
+    JLabel calcText = new JLabel("0");
+    calcText.setBounds(15, 100, windowWidth - 30, 50);
+    calcText.setFont(new Font("SansSerif", Font.PLAIN, 30));
+    frame.add(calcText);
 
     // buttons
-    JButton button1 = new JButton("Add");
-    button1.setBounds(15, 200, 100, 50);
-    frame.add(button1);
+    int buttonWidth = 100;
+    int buttonHeight = 50;
+
+    JButton button11 = new JButton("\u2190");
+    button11.setBounds(15, 200, buttonWidth, buttonHeight);
+    frame.add(button11);
+
+    JButton button12 = new JButton("CE");
+    button12.setBounds(15 * 2 + buttonWidth, 200, buttonWidth, buttonHeight);
+    frame.add(button12);
 
     // button functionality
-    button1.addActionListener(e -> {
+    button11.addActionListener(e -> {
       title.setText("Button clicked!");
     });
 
